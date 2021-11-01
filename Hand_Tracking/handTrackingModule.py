e4c83d6d6a283b310b659cd9765fbe029220a8cb
erichan <erichan2046@qq.com>
2021-11-01 19:46:42 +0800

    init_commit

--- Hand_Tracking/handTrackingModule.py
+++ Hand_Tracking/handTrackingModule.py
@@ -2,7 +2,7 @@ import cv2
 import mediapipe as mp
 import time
 class handDetector():
-    def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
+    def __init__(self, mode=False, maxHands=2, detectionCon=0.7, trackCon=0.7):
         self.mode = mode
         self.maxHands = maxHands
         self.detectionCon = detectionCon
--- mouse/HandTrackingModule.py
+++ mouse/HandTrackingModule.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 class handDetector():
-    def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
+    def __init__(self, mode=False, maxHands=2, detectionCon=0.7, trackCon=0.7):
         self.mode = mode
         self.maxHands = maxHands
         self.detectionCon = detectionCon
